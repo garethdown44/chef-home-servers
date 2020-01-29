2f1405fa012d9043cf551d2ffc6f51763a11083b
Gareth Down <g@garethdown.com>
2020-01-29 09:23:22 +0000

    add samba share

--- nas-01.rb
+++ nas-01.rb
@@ -21,3 +21,23 @@ execute 'apt-get-update' do
 end
 
 apt_package 'openssh-server'
+
+samba_user 'user1' do
+  password 'user1' # user password for samba and the system
+  # comment 'user_name_comment'
+  home '/mnt/data-2' # Users home.
+  shell '/bin/zsh' # User shell to set, e.g. /bin/sh, /sbin/nologin
+  manage_home true # true/false, whether to manage the users home directory location
+end
+
+samba_server 'samba server' do
+  hosts_allow 192.168.1.0/24
+end
+
+samba_share 'data-2' do
+  comment
+  guest_ok yes
+  write_list user1
+  valid_users user1
+  path /mnt/data-2
+end
